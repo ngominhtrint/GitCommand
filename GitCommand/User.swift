f1e373ab78b5d56d98803ff8e849fc69ea01d02a
tri.ngo <ngominhtrint@gmail.com>
2020-08-07 15:19:19 +0800

    Add struct of User.

---
+++ GitCommand/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  GitCommand
+//
+//  Created by TriNgo on 8/7/20.
+//  Copyright © 2020 RoverDream. All rights reserved.
+//
+
+import Foundation
+
+struct User {
+    
+    
+}
--- GitCommand/ViewController.swift
+++ GitCommand/ViewController.swift
@@ -21,5 +21,6 @@ class ViewController: UIViewController {
         print("Checkout new branch: feature1")
     }
 
+    
 }
 
